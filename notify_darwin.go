63804c4c98385b5d430420ada54ad149935e6a74
dilip.singh <dilip.singh@servicenow.com>
2026-06-04 22:33:25 +0530

    fix: replace AppleScript notification with NSUserNotificationCenter (shows in foreground)

--- notify_darwin.go
+++ notify_darwin.go
@@ -9,15 +9,33 @@ package core
 #import <Cocoa/Cocoa.h>
 #include <stdlib.h>
 
+// Delegate that forces banners to appear even when the app is focused.
+@interface SDNotifDelegate : NSObject <NSUserNotificationCenterDelegate>
+@end
+@implementation SDNotifDelegate
+- (BOOL)userNotificationCenter:(NSUserNotificationCenter *)center
+     shouldPresentNotification:(NSUserNotification *)notification {
+	return YES;
+}
+@end
+
+static SDNotifDelegate *_notifDelegate = nil;
+
 void macNotify(const char *title, const char *body) {
-	@autoreleasepool {
-		NSString *t = [NSString stringWithUTF8String:title];
-		NSString *b = [NSString stringWithUTF8String:body];
-		NSString *src = [NSString stringWithFormat:
-			@"display notification \"%@\" with title \"%@\" sound name \"Glass\"", b, t];
-		NSAppleScript *script = [[NSAppleScript alloc] initWithSource:src];
-		[script executeAndReturnError:nil];
-	}
+	dispatch_async(dispatch_get_main_queue(), ^{
+		@autoreleasepool {
+			NSUserNotificationCenter *center = [NSUserNotificationCenter defaultUserNotificationCenter];
+			if (_notifDelegate == nil) {
+				_notifDelegate = [[SDNotifDelegate alloc] init];
+				center.delegate = _notifDelegate;
+			}
+			NSUserNotification *n = [[NSUserNotification alloc] init];
+			n.title = [NSString stringWithUTF8String:title];
+			n.informativeText = [NSString stringWithUTF8String:body];
+			n.soundName = NSUserNotificationDefaultSoundName;
+			[center deliverNotification:n];
+		}
+	});
 }
 
 // macConsentDialog shows a modal NSAlert on the main thread with Accept/Reject
