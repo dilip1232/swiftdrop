0f130cc8bcb0cb35df6913e534fe791345c7818d
dilip.singh <dilip.singh@servicenow.com>
2026-06-04 22:59:44 +0530

    fix: use UNUserNotificationCenter for macOS notifications
    NSUserNotificationCenter is dead on macOS 26+. Switch to the modern
    UserNotifications framework with a delegate that forces banners to appear
    even when the app window is in the foreground.

--- notify_darwin.go
+++ notify_darwin.go
@@ -4,37 +4,58 @@ package core
 
 /*
 #cgo CFLAGS: -x objective-c
-#cgo LDFLAGS: -framework Foundation -framework Cocoa
+#cgo LDFLAGS: -framework Foundation -framework Cocoa -framework UserNotifications
 #import <Foundation/Foundation.h>
 #import <Cocoa/Cocoa.h>
+#import <UserNotifications/UserNotifications.h>
 #include <stdlib.h>
 
-// Delegate that forces banners to appear even when the app is focused.
-@interface SDNotifDelegate : NSObject <NSUserNotificationCenterDelegate>
+// Delegate: show banner+sound even when the app is in the foreground.
+@interface SDNotifDelegate : NSObject <UNUserNotificationCenterDelegate>
 @end
 @implementation SDNotifDelegate
-- (BOOL)userNotificationCenter:(NSUserNotificationCenter *)center
-     shouldPresentNotification:(NSUserNotification *)notification {
-	return YES;
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+       willPresentNotification:(UNNotification *)notification
+         withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler {
+	completionHandler(UNNotificationPresentationOptionBanner | UNNotificationPresentationOptionSound);
 }
 @end
 
 static SDNotifDelegate *_notifDelegate = nil;
+static BOOL _authorized = NO;
+
+static void _setupDelegate(void) {
+	if (_notifDelegate) return;
+	_notifDelegate = [[SDNotifDelegate alloc] init];
+	[UNUserNotificationCenter currentNotificationCenter].delegate = _notifDelegate;
+}
+
+static void _deliver(NSString *t, NSString *b) {
+	UNMutableNotificationContent *content = [[UNMutableNotificationContent alloc] init];
+	content.title = t;
+	content.body = b;
+	content.sound = [UNNotificationSound defaultSound];
+	NSString *ident = [NSString stringWithFormat:@"sd-%f", [[NSDate date] timeIntervalSince1970]];
+	UNNotificationRequest *req = [UNNotificationRequest requestWithIdentifier:ident content:content trigger:nil];
+	[[UNUserNotificationCenter currentNotificationCenter] addNotificationRequest:req withCompletionHandler:nil];
+}
 
 void macNotify(const char *title, const char *body) {
+	NSString *t = [[NSString alloc] initWithUTF8String:title];
+	NSString *b = [[NSString alloc] initWithUTF8String:body];
+
 	dispatch_async(dispatch_get_main_queue(), ^{
-		@autoreleasepool {
-			NSUserNotificationCenter *center = [NSUserNotificationCenter defaultUserNotificationCenter];
-			if (_notifDelegate == nil) {
-				_notifDelegate = [[SDNotifDelegate alloc] init];
-				center.delegate = _notifDelegate;
-			}
-			NSUserNotification *n = [[NSUserNotification alloc] init];
-			n.title = [NSString stringWithUTF8String:title];
-			n.informativeText = [NSString stringWithUTF8String:body];
-			n.soundName = NSUserNotificationDefaultSoundName;
-			[center deliverNotification:n];
+		_setupDelegate();
+		if (_authorized) {
+			_deliver(t, b);
+			return;
 		}
+		UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+		[center requestAuthorizationWithOptions:(UNAuthorizationOptionAlert | UNAuthorizationOptionSound | UNAuthorizationOptionBadge)
+			completionHandler:^(BOOL granted, NSError *error) {
+				_authorized = granted;
+				if (granted) _deliver(t, b);
+			}];
 	});
 }
 
